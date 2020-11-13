9212558a122fba3508abe470f7f480e41f05ea98
彭建超 <502504318@qq.com>
2020-11-13 17:34:04 +0800

    项目初始化

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const options = require('./src/options')
 
 cmd.command('create').description('初始化模板').action(async (args) => {
     typeof args !== 'string' && (console.log(chalk.red('缺少必填参数')), process.exit(1))
-    console.log(chalk.yellow('zjs脚手架初始化模板，不要问我为什么是中文的，因为我英语不好 \n'))
+    console.log(chalk.yellow('pjc脚手架初始化模板，不要问我为什么是中文的，因为我英语不好 \n'))
     // 填选项
     await options()
     // 拉取
